b1445286f95a800c1df56c9c8775ae2fba5ee507
MalshiUdesika <malshigamage15.com>
2025-08-05 16:07:26 +0530

    first init

---
+++ script.js
@@ -0,0 +1,26 @@
+/*document.addEventListener('DOMContentLoaded' , function()) {
+    //set current year in footer
+    document.getElementById('year').textContent = new Date().getFullYear();
+
+    //mobile menu toggle
+    const mobileMenuBtn = document.querySelector(' .mobile-menu');
+    const nav = document.querySelector('nav');
+
+    mobileMenuBtn.addEventListener('click',function(){
+        nav.classList.toggle('active');
+        this.querySelector('i').classList.toggle('fa-times');
+    });
+}
+
+//header scroll effect
+window.addEventListener('scroll', function(){
+    const header = this.document.querySelector('header');
+    header.classList.toggle('scrolled', this.window.scrollY > 50);
+});
+
+//Skills data 
+const skillsData = [
+    { icon: 'fab fa-html5', name:'HTML5'},
+    {icon: 'fab fa-css3-alt', name:'CSS3'},
+    {icon: 'fab fa-js', name:'JavaScript'}
+]; */
