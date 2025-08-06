4757fb17dc5f348f75fa66cc719d1da3b4aec3f6
MalshiUdesika <malshigamage15.com>
2025-08-06 18:49:24 +0530

    add darkmode button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*document.addEventListener('DOMContentLoaded' , function()) {
+document.addEventListener('DOMContentLoaded' , function()) {
     //set current year in footer
     document.getElementById('year').textContent = new Date().getFullYear();
 
@@ -23,4 +23,22 @@ const skillsData = [
     { icon: 'fab fa-html5', name:'HTML5'},
     {icon: 'fab fa-css3-alt', name:'CSS3'},
     {icon: 'fab fa-js', name:'JavaScript'}
-]; */
+]; 
+
+
+//dark mde 
+  const toggleBtn = document.getElementById("darkModeToggle");
+  const body = document.body;
+
+  toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    // Change icon when toggled
+    if (body.classList.contains("dark-mode")) {
+      toggleBtn.textContent = "☀️"; // Light mode icon
+    } else {
+      toggleBtn.textContent = "🌙"; // Dark mode icon
+    }
+  });
+
+
